455e74576d7a0de4ea65ebf02d2c9ddf6ae9f574
Paul Solt <PaulSolt@gmail.com>
2019-12-12 17:26:57 -0500

    Updated to respond to presentation changes when the popover is dismissed by tapping off the popover, or if the iOS13 modal style dialog is dismissed

--- Planets/View Controllers/PlanetsCollectionViewController.swift
+++ Planets/View Controllers/PlanetsCollectionViewController.swift
@@ -21,16 +21,45 @@ class PlanetsCollectionViewController: UICollectionViewController {
         return shouldShowPluto ? planetController.planetsWithPluto : planetController.planetsWithoutPluto
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		updateUI()
+	}
+	
+	func updateUI() {
         collectionView?.reloadData()
-    }
+	}
     
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSettings" {
+			guard let detailVC = segue.destination as? SettingsViewController else { return }
+            
+			// Configure the popover on iPhone to not be fullscreen
+			detailVC.popoverPresentationController?.delegate = self
+            
+			// Listen for popover or iOS13 modal is dismissed notifications
+			detailVC.presentationController?.delegate = self
+        }
+        
+        if segue.identifier == "ShowPlanetDetail" {
+            guard let indexPath = collectionView?.indexPathsForSelectedItems?.first else { return }
+			
+			guard let detailVC = segue.destination as? PlanetDetailViewController else { return }
+            detailVC.planet = planets[indexPath.row]
+        }
+    }
+	
     @IBAction func unwindToPlanetsCollectionViewController(_ sender: UIStoryboardSegue) {
-    }
-    
-    // MARK: UICollectionViewDataSource
-    
+		// Update the UI if we segue back to this view controller
+		updateUI()
+	}
+}
+
+// MARK: UICollectionViewDataSource
+extension PlanetsCollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return planets.count
     }
@@ -44,32 +73,20 @@ class PlanetsCollectionViewController: UICollectionViewController {
         
         return cell
     }
-    
-    // MARK: - Navigation
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ShowSettings" {
-            let detailVC = segue.destination
-            let ppc = detailVC.popoverPresentationController
-            if let button = sender as? UIButton {
-                ppc?.sourceView = button
-                ppc?.sourceRect = button.bounds
-                ppc?.backgroundColor = .black
-            }
-            ppc?.delegate = self
-        }
-        
-        if segue.identifier == "ShowPlanetDetail" {
-            guard let indexPath = collectionView?.indexPathsForSelectedItems?.first else { return }
-            let detailVC = segue.destination as! PlanetDetailViewController
-            detailVC.planet = planets[indexPath.row]
-        }
-    }
 }
 
 extension PlanetsCollectionViewController: UIPopoverPresentationControllerDelegate {
-    
+    // We can "force" an iPhone to display an iPad-like popover by changing the model
+	// presentation style in code.
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
     }
+}
+
+extension PlanetsCollectionViewController: UIAdaptivePresentationControllerDelegate {
+	func presentationControllerWillDismiss(_ presentationController: UIPresentationController) {
+		// Every time the user goes to swipe down to dismiss the modal
+		// popover, we should update the UI based on the new state
+		updateUI()
+	}
 }
